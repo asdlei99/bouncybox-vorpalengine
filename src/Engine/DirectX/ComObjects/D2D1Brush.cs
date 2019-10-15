c24fc5b29779c57a762640d8debf0c3298a90155
Nathan Alden, Sr <1189759+nathan-alden-sr@users.noreply.github.com>
2019-10-15 17:02:32 -0400

    Closes #9196 - Remove ID2D1Brush

--- src/Engine/DirectX/ComObjects/D2D1Brush.cs
+++ src/Engine/DirectX/ComObjects/D2D1Brush.cs
@@ -1,25 +1,28 @@
 ﻿using BouncyBox.VorpalEngine.Engine.Interop;
+using TerraFX.Interop;
 
 namespace BouncyBox.VorpalEngine.Engine.DirectX.ComObjects
 {
-    /// <inheritdoc cref="ID2D1Brush" />
-    public unsafe class D2D1Brush : ComObject<TerraFX.Interop.ID2D1Brush>, ID2D1Brush
+    /// <inheritdoc />
+    public unsafe class D2D1Brush : ComObject<ID2D1Brush>
     {
-        private ComPtr<TerraFX.Interop.ID2D1Brush> _d2d1Brush;
+        private ComPtr<ID2D1Brush> _d2d1Brush;
 
         /// <inheritdoc />
         /// <summary>
         ///     Initializes a new instance of the <see cref="D2D1Brush" /> type.
         /// </summary>
-        public D2D1Brush(ComPtr<TerraFX.Interop.ID2D1Brush> d2d1Brush)
+        public D2D1Brush(ComPtr<ID2D1Brush> d2d1Brush)
         {
             _d2d1Brush = d2d1Brush;
         }
 
         /// <inheritdoc />
-        public override TerraFX.Interop.ID2D1Brush* Pointer => _d2d1Brush;
+        public override ID2D1Brush* Pointer => _d2d1Brush;
 
-        /// <inheritdoc />
+        /// <summary>
+        ///     Proxies <see cref="ID2D1Brush.GetOpacity" /> and <see cref="ID2D1Brush.SetOpacity" />.
+        /// </summary>
         public float Opacity
         {
             get => Pointer->GetOpacity();
